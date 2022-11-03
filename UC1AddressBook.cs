2c398574d109c0e434387a5a13d35e032666e3e4
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-11-03 22:26:12 +0530

    Create a address book

---
+++ UC1AddressBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23AddressBook
+{
+    internal class UC1AddressBook
+    {
+        List<string> firstName = new List<string>();
+        List<string> lastName = new List<string>();
+        List<string> city = new List<string>();
+        List<string> state = new List<string>();
+        List<int> zip = new List<int>();
+        List<int> phoneNumber = new List<int>();
+        List<string> email = new List<string>();
+    }
+}
